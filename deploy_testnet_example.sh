c1c9fa4b806ecaec2bcf953ba8acf8b50560fd1f
Nino Lipartiia <nino.lipartiya@gmail.com>
2022-07-28 13:44:05 +0300

    Bank messages

--- deploy_testnet_example.sh
+++ deploy_testnet_example.sh
@@ -6,7 +6,7 @@ cargo wasm
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/rust-optimizer:0.12.6
+  cosmwasm/rust-optimizer-arm64:0.12.6
 
 NODE="--node https://rpc.uni.juno.deuslabs.fi:443"
 TXFLAG="--node https://rpc.uni.juno.deuslabs.fi:443 --chain-id uni-3 --gas-prices 0.025ujunox --gas auto --gas-adjustment 1.3 --broadcast-mode block"
@@ -17,7 +17,7 @@ AGENT="$2"
 USER="$3"
 
 # In case of M1 MacBook replace cw_croncat.wasm with cw_croncat-aarch64.wasm 
-RES=$(junod tx wasm store artifacts/cw_croncat.wasm --from $OWNER $TXFLAG -y --output json -b block)
+RES=$(junod tx wasm store artifacts/cw_croncat-aarch64.wasm --from $OWNER $TXFLAG -y --output json -b block)
 CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
 
 # Instantiate
@@ -30,11 +30,19 @@ CONTRACT=$(junod query wasm list-contract-by-code $CODE_ID $NODE --output json |
 REGISTER_AGENT='{"register_agent":{}}'
 junod tx wasm execute $CONTRACT "$REGISTER_AGENT" --from $AGENT $TXFLAG -y
 
-# Create a task
-STAKE='{"create_task":{"task":{"interval":"Immediate","boundary":{},"stop_on_fail":false,"actions":[{"msg":{"staking":{"delegate":{"validator":"juno14vhcdsyf83ngsrrqc92kmw8q9xakqjm0ff2dpn","amount":{"denom":"ujunox","amount":"1000000"}}}},"gas_limit":150000}],"rules":null}}}'
+# Create tasks for staking, sending and burning tokens
+STAKE='{"create_task":{"task":{"interval":"Once","boundary":null,"stop_on_fail":false,"actions":[{"msg":{"staking":{"delegate":{"validator":"juno14vhcdsyf83ngsrrqc92kmw8q9xakqjm0ff2dpn","amount":{"denom":"ujunox","amount":"1000000"}}}},"gas_limit":150000}],"rules":null}}}'
 junod tx wasm execute $CONTRACT "$STAKE" --amount 1000000ujunox --from $USER $TXFLAG -y
+
+BANK_SEND='{"create_task":{"task":{"interval":"Once","boundary":null,"stop_on_fail":false,"actions":[{"msg":{"bank":{"send":{"to_address":"juno1e48ptufsh6yplls6vmy4827uxnxlmpdul0nqkg","amount":[{"denom":"ujunox","amount":10000}]}}},"gas_limit":150000}],"rules":null}}}'
+junod tx wasm execute $CONTRACT "$BANK_SEND" --amount 1000000ujunox --from $USER $TXFLAG -y
+
+BANK_BURN='{"create_task":{"task":{"interval":"Once","boundary":null,"stop_on_fail":false,"actions":[{"msg":{"bank":{"burn":{"amount":[{"denom":"ujunox","amount":10000}]}}},"gas_limit":150000}],"rules":null}}}'
+junod tx wasm execute $CONTRACT "$BANK_BURN" --amount 1000000ujunox --from $USER $TXFLAG -y
 
 # proxy_call
 sleep 10      # is needed to make sure this call in the next block 
 PROXY_CALL='{"proxy_call":{}}'
+junod tx wasm execute $CONTRACT "$PROXY_CALL" --from $AGENT $TXFLAG -y
+junod tx wasm execute $CONTRACT "$PROXY_CALL" --from $AGENT $TXFLAG -y
 junod tx wasm execute $CONTRACT "$PROXY_CALL" --from $AGENT $TXFLAG -y
